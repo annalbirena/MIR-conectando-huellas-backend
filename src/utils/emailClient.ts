0c2347bf50b1dabea28ee21a706aed584bec5c37
Anna <ana84168@gmail.com>
2024-11-23 15:46:42 -0500

    fix: email template

--- src/utils/emailClient.ts
+++ src/utils/emailClient.ts
@@ -1,7 +1,7 @@
 import { createTransport } from 'nodemailer';
-import Handlebars from 'handlebars';
+/* import Handlebars from 'handlebars';
 import fs from 'fs';
-import path from 'path';
+import path from 'path'; */
 
 const transporter = createTransport({
   host: 'smtp.gmail.com',
@@ -27,24 +27,29 @@ export const sendAccountConfirmationEmail = async (
   to: string,
   token: string,
 ) => {
-  /* const verificationUrl = `${process.env.DEPLOY_URL}api/users/verify/${token}`; */
   const verificationUrl = `${process.env.FRONTEND_VERIFY_URL}/${token}`;
 
-  const source = fs.readFileSync(
+  /* const source = fs.readFileSync(
     path.join(__dirname, '../templates/validateAccountTemplate.hbs'),
     'utf8',
-  );
+  ); */
 
-  const template = Handlebars.compile(source);
+  /* const template = Handlebars.compile(source);
   const replacements = {
     name: name,
     email: to,
     verificationUrl: verificationUrl,
-  };
-  const htmlToSend = template(replacements);
+  }; */
+  /* const htmlToSend = template(replacements); */
 
   const subject = 'Confirma tu cuenta';
-  const html = htmlToSend;
+  /*  const html = htmlToSend; */
+  const html = `
+    <h1>Conectando Huellas</h1>
+    <h3>¡Bienvenid@ a nuestra comunidad ${name}!</h3>
+    <p>Para continuar, debemos verificar que eres el(la) propietario(a) de esta dirección de correo electrónico ${to}</p>
+    <p>Por favor, confirma tu cuenta haciendo click <a target="_blank" href="${verificationUrl}">aquí</a></p>
+  `;
 
   await sendEmail(to, subject, html);
 };
